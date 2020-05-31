d7d60dc7a76adb45c67c761ff2f6c2158de562c5
D Simpson <simpson-david@hotmail.com>
2020-05-31 21:06:39 +0100

    Creation of simple BeatSequenceTimeRepresentation, in progress (untested)

---
+++ src/libraries/BeatSequence.js
@@ -0,0 +1,128 @@
+export { BeatSequenceTimeRepresentation, BeatTimeRepresentation }
+
+class BeatSequenceTimeRepresentation {
+  /**
+   *
+   * @param {Array.<BeatTimeRepresentation>} beats
+   */
+  constructor(beats) {
+    this.beats = beats
+  }
+
+  /*********************************************************
+   * PUBLIC METHODS
+   *********************************************************/
+  /**
+   *
+   * @param {Number} position integer indicating the bar number to trim the sequence to, so position becomes "bar 1" of
+   * the new BSTR; must be <= the total bar count
+   * @returns {BeatSequenceTimeRepresentation} the BSTR resulting from removing all bars numbered up to, but not including, position
+   * @public
+   */
+  trim(position) {
+    return new BeatSequenceTimeRepresentation(this.beats.filter(beat => beat.associatedBarNumber >= position))
+  }
+
+  /**
+   *
+   * @param {Number} countInLength the number of times to replicate the first bar-worth of beats of the BSTR
+   * @returns {BeatSequenceTimeRepresentation} a new BSTR, with the first bar-worth of beats of this BSTR replicated barCount more times and added to the front
+   * @public
+   */
+  addCountIn(countInLength) {
+    let countInBar = this.getFirstBarCountIn()
+    let fullCountIn = this.repeatArray(countInLength, countInBar)
+    return new BeatSequenceTimeRepresentation(fullCountIn.concat(this.beats))
+  }
+
+  /**
+   *
+   * @param {Number} index get the beat at the given index (0-indexing)
+   * @returns {BeatTimeRepresentation}
+   * @public
+   */
+  getBeat(index) {
+    return this.beats[index]
+  }
+
+  /**
+   *
+   * @param {Number} index the index of the beat to check
+   * @returns {Boolean} whether or not the beat at this index is the final beat
+   * @public
+   */
+  isFinalBeat(index) {
+    return index === this.beats.length - 1
+  }
+
+  /**********************************************
+   * PRIVATE METHODS
+   **********************************************/
+  /**
+   * @returns {Array.<BeatTimeRepresentation>} the first bar of the beats, in order, as a single-bar count-in
+   * @private
+   */
+  getFirstBarCountIn() {
+    // relying on filtering being in-order, but should be fine
+    return this.beats.filter(beat => beat.associatedBarNumber === 1).map(beat => new BeatTimeRepresentation(beat.durationInMillis, true, beat.isFirstBeatOfBar, beat.associatedBarNumber))
+  }
+
+  /**
+   * @description Repeat the given array a a number of times given by times, concatenate the results.
+   * This is a shallow copy - any changes to an element of one array will be reflected in the other
+   * repeats.
+   * @template T
+   * @param {Number} times number of times to repeat the array's contents
+   * @param {Array.<T>} a array to repeat
+   * @returns {Array.<T>} the array formed from {@link times} repetitions of {@link a}'s contents in order
+   * @private
+   */
+  repeatArray(times, a) {
+    return new Array(times).fill(a).flat()
+  }
+}
+
+class BeatTimeRepresentation {
+  /**
+   * @description Representation of a beat using a time duration
+   * @param {Number} durationInMillis duration of the beat in milliseconds
+   * @param {Boolean} isCountIn is this beat part of a count-in?
+   * @param {Boolean} isFirstBeatOfBar  is this beat the first one in its bar?
+   * @param {Number} associatedBarNumber the bar number this beat comes from
+   */
+  constructor(durationInMillis, isCountIn, isFirstBeatOfBar, associatedBarNumber) {
+    this._durationInMillis = durationInMillis
+    this._isCountIn = isCountIn
+    this._isFirstBeatOfBar = isFirstBeatOfBar
+    this._associatedBarNumber = associatedBarNumber
+  }
+
+
+  /**
+   * @returns {Number}
+   */
+  get durationInMillis() {
+    return this._durationInMillis
+  }
+
+  /**
+   * @returns {Boolean}
+   */
+  get isCountIn() {
+    return this._isCountIn
+  }
+
+  /**
+   * @returns {Boolean}
+   */
+  get isFirstBeatOfBar() {
+    return this._isFirstBeatOfBar
+  }
+
+  /**
+   * @returns {Number}
+   */
+  get associatedBarNumber() {
+    return this._associatedBarNumber
+  }
+}
--- src/libraries/DomainModel.js
+++ src/libraries/DomainModel.js
@@ -1,41 +1,47 @@
-export { TimeSignature, BPM, BarSequence, BasicDuration };
+import { BeatSequenceTimeRepresentation, BeatTimeRepresentation } from "@/libraries/BeatSequence.js"
+
+export { TimeSignature, BPM, BarSequence, BasicDuration, SimpleBeatSequenceCreator }
 
 class TimeSignature {
   constructor(numerator, denominator) {
-    this.numerator = numerator;
-    this.denominator = denominator;
+    this.numerator = numerator
+    this.denominator = denominator
   }
 
   getNumerator() {
-    return this.numerator;
+    return this.numerator
   }
 
   getDenominatorAsNumber() {
-    return BasicDuration.toInteger(this.denominator);
+    return BasicDuration.toInteger(this.denominator)
   }
 
   copy() {
-    return new TimeSignature(this.numerator, this.denominator);
+    return new TimeSignature(this.numerator, this.denominator)
   }
 }
 class BPM {
   constructor(perMinute, denominator) {
-    this.perMinute = perMinute;
-    this.denominator = denominator;
+    this.perMinute = perMinute
+    this.denominator = denominator
+  }
+
+  getPerMinute() {
+    return this.perMinute
   }
 
   copy() {
-    return new BPM(this.perMinute, this.denominator);
+    return new BPM(this.perMinute, this.denominator)
   }
 }
 class BarSequence {
   constructor() {
-    this.bars = [];
+    this.bars = []
   }
 
   addBarsToEnd(timeSig, bpm, count) {
     for (let i = 1; i <= count; i++) {
-      this.bars.push(new Bar(timeSig, bpm));
+      this.bars.push(new Bar(timeSig, bpm))
     }
   }
 
@@ -44,32 +50,42 @@ class BarSequence {
   }
 
   getBarCount() {
-    return this.bars.length;
+    return this.bars.length
   }
 
   getTimeSigOf(barNumber) {
-    return this.bars[barNumber - 1].getTimeSig();
+    return this.bars[barNumber - 1].getTimeSig()
   }
   getTempoOf(barNumber) {
-    return this.bars[barNumber - 1].getTempo();
+    return this.bars[barNumber - 1].getTempo()
   }
   deleteBar(barNumber) {
     this.bars = this.bars.filter((bar, index) => {
-      return index != barNumber - 1;
-    });
+      return index != barNumber - 1
+    })
+  }
+
+  // JSDoc doesn't allow imports to be treated as types, hence the explicit name below of BSTR
+  /**
+   *
+   * @param {IBeatSequenceCreator} beatSequenceCreator
+   * @returns {BeatSequenceTimeRepresentation} BeatSequenceTimeRepresentation
+   */
+  getTimeRepresentation(beatSequenceCreator) {
+    return beatSequenceCreator.createFrom(this)
   }
 }
 class Bar {
   constructor(timeSig, tempo) {
-    this.timeSig = timeSig;
-    this.tempo = tempo;
+    this.timeSig = timeSig
+    this.tempo = tempo
   }
 
   getTimeSig() {
-    return this.timeSig.copy();
+    return this.timeSig.copy()
   }
   getTempo() {
-    return this.tempo.copy();
+    return this.tempo.copy()
   }
 }
 const BasicDuration = {
@@ -82,24 +98,99 @@ const BasicDuration = {
   fromInteger(i) {
     switch (i) {
       case 1:
-        return BasicDuration._1st;
+        return BasicDuration._1st
       case 2:
-        return BasicDuration._2nd;
+        return BasicDuration._2nd
       case 4:
-        return BasicDuration._4th;
+        return BasicDuration._4th
       case 8:
-        return BasicDuration._8th;
+        return BasicDuration._8th
       case 16:
-        return BasicDuration._16th;
+        return BasicDuration._16th
       case 32:
-        return BasicDuration._32nd;
+        return BasicDuration._32nd
       default:
-        throw "Tried to get a basic duration of " +
-          i +
-          " which is not one of 1,2,4,8,16,32";
+        throw "Tried to get a basic duration of " + i + " which is not one of 1,2,4,8,16,32"
     }
   },
   toInteger(bd) {
-    return Math.pow(2, bd);
+    return Math.pow(2, bd)
   },
-};
+}
+
+// Slightly different way of having an explicit interface, since JSDoc isn't working at the moment
+class IBeatSequenceCreator {
+  constructor() {}
+  /**
+   *
+   * @param {BarSequence} barSequence
+   * @returns {BeatSequenceTimeRepresentation}
+   */
+  createFrom(barSequence) {
+    throw "Not implemented"
+  }
+}
+
+class SimpleBeatSequenceCreator extends IBeatSequenceCreator {
+  /**
+   *
+   * @param {BarSequence} barSequence
+   * @returns {BeatSequenceTimeRepresentation} BeatSequenceTimeRepresentation
+   */
+  createFrom(barSequence) {
+    let beats = barSequence.bars.map((bar, i) => this.beatsFromBar(bar, i + 1)).flat()
+    return new BeatSequenceTimeRepresentation(beats)
+  }
+
+  /**
+   *
+   * @param {Bar} bar
+   * @returns {Array.<BeatTimeRepresentation>}
+   */
+  beatsFromBar(bar, barNumber) {
+    let tempo = bar.getTempo()
+    let timeSig = bar.getTimeSig()
+
+    /*
+    examples (tempo is BPM,denominator)
+    what happens when denominators equal?
+    tempo=120,4; timeSig=4/4, each 4 lasts 60/120 seconds, there are 4 per bar
+    tempo=150,4; timeSig=2/4, each 4 lasts 60/150 seconds, there are 2 per bar
+    tempo=30,4; timeSig=3/4, each 4 lasts 60/30 seconds, there are 3 per bar
+    
+    in general: number of seconds delay per tempo subdivision= 60/tempo.bpm
+    
+    what happens when denominators differ?
+    tempo=120,4 timeSig=4/8, each 4 lasts 60/120 seconds, each 8 lasts 30/120 seconds
+    tempo=120,2 timeSig=4/8, each 2 lasts 60/120 seconds, each 8 lasts 15/120 seconds
+    tempo=240,4 timeSig=2/2, each 4 lasts 60/240 seconds, each 2 lasts 120/240 seconds
+
+    let tempo subdivision be t, timeSig subdivision be s
+    ratio (t/s) is number of tempo subdivisions per time sig subdivision
+    t/s= 1: same
+    t/s > 1: multiple tempo subdivs per time sig subdiv
+    t/s < 1: multiple time sig subdivs per tempo subdiv
+
+    quick fractional arithmetic: then (60/tempo.bpm) * (t/s) gives
+    number of seconds delay per time sig subdivision
+
+    check: 
+      tempo=120,2 timeSig=4/8, each 2 lasts 60/120 seconds; (60/120) * (2/8) = 15/120 for each 8 
+      tempo=240,4 timeSig=2/2, each 4 lasts 60/240 seconds; (60/240) * (4/2) = 120/240 for each 2
+      seems to work!
+
+    then just *1000 for millis duration
+    */
+    let tsRatio = tempo.getDenominatorAsNumber() / timeSig.getDenominatorAsNumber()
+    let timeRatio = 60 / tempo.getPerMinute()
+    let millisPerSubdivision = timeRatio * tsRatio * 1000
+
+    let beats = new Array(timeSig.getNumerator())
+      .fill(null)
+      .map((_, i) => 
+        new BeatTimeRepresentation(millisPerSubdivision, false, i === 0, barNumber)
+        )
+
+    return beats
+  }
+}
