162b2d1131c3caa5999372329b9e855ae7997ba9
D Simpson <simpson-david@hotmail.com>
2020-06-16 18:26:08 +0100

    Additional test file completes tests, contains no useful tests yet

---
+++ tests/DomainModel.test.js
@@ -0,0 +1,7 @@
+import { BarSeqence } from "@/libraries/DomainModel.js"
+import { BeatSequenceTimeRepresentation, BeatTimeRepresentation } from "@/libraries/BeatSequence.js"
+
+
+test("BSTR of a single 4/4 bar at 1/4=120", () => {
+  expect(1).toBe(1)
+})
